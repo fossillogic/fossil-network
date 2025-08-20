1fde19259763a881cbff6f080b859254ea3716ce
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-08-20 15:10:23 -0700

    Update mail.c

--- code/logic/mail.c
+++ code/logic/mail.c
@@ -16,6 +16,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef NOMINMAX
+    #define NOMINMAX
+    #endif
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <windows.h>
+    #include <iphlpapi.h>
+#else
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <netdb.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+#endif
+
 // ------------------------------
 // SMTP (send a simple email)
 // ------------------------------
