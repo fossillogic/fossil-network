8eea4d1666c8979ca69d98a0d65b291fc7c0a20d
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-08-27 08:44:21 -0700

    Update dns.c

--- code/logic/dns.c
+++ code/logic/dns.c
@@ -12,3 +12,66 @@
  * -----------------------------------------------------------------------------
  */
 #include "fossil/network/dns.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+#else
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netdb.h>
+  #include <arpa/inet.h>
+#endif
+
+int fossil_network_dns_resolve(const char *host,
+                               char addrs[][64],
+                               int max_addrs) {
+    if (!host || !addrs || max_addrs <= 0) {
+        return -1;
+    }
+
+    struct addrinfo hints;
+    struct addrinfo *res = NULL, *p;
+    int count = 0;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;     // allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // doesn't matter, just want addresses
+
+    int rv = getaddrinfo(host, NULL, &hints, &res);
+    if (rv != 0) {
+#ifdef _WIN32
+        fprintf(stderr, "DNS resolve failed for %s: %d\n", host, WSAGetLastError());
+#else
+        fprintf(stderr, "DNS resolve failed for %s: %s\n", host, gai_strerror(rv));
+#endif
+        return -1;
+    }
+
+    for (p = res; p != NULL && count < max_addrs; p = p->ai_next) {
+        void *addr_ptr = NULL;
+
+        if (p->ai_family == AF_INET) {
+            // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr_ptr = &(ipv4->sin_addr);
+        } else if (p->ai_family == AF_INET6) {
+            // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr_ptr = &(ipv6->sin6_addr);
+        }
+
+        if (addr_ptr) {
+            if (inet_ntop(p->ai_family, addr_ptr, addrs[count], 64) != NULL) {
+                count++;
+            }
+        }
+    }
+
+    freeaddrinfo(res);
+    return count; // number of resolved addresses
+}
