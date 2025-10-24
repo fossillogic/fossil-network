22210e97259b4044b0037726366d1edd1389b009
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-10-23 20:24:28 -0500

    update test cases

--- code/tests/cases/test_cluster.cpp
+++ code/tests/cases/test_cluster.cpp
@@ -22,13 +22,13 @@
 // mock objects are set here.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
-FOSSIL_TEST_SUITE(c_cluster_fixture);
+FOSSIL_TEST_SUITE(cpp_cluster_fixture);
 
-FOSSIL_SETUP(c_cluster_fixture) {
+FOSSIL_SETUP(cpp_cluster_fixture) {
     // Setup the test fixture
 }
 
-FOSSIL_TEARDOWN(c_cluster_fixture) {
+FOSSIL_TEARDOWN(cpp_cluster_fixture) {
     // Teardown the test fixture
 }
 
@@ -40,137 +40,137 @@ FOSSIL_TEARDOWN(c_cluster_fixture) {
 // as samples for library usage.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
-FOSSIL_TEST_CASE(c_cluster_test_join_with_valid_self_and_seeds) {
+FOSSIL_TEST_CASE(cpp_cluster_test_join_with_valid_self_and_seeds) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[2] = {
         { "node2", "127.0.0.2", 9002 },
         { "node3", "127.0.0.3", 9003 }
     };
-    int result = fossil_network_cluster_join(&self, seeds, 2);
+    int result = fossil::network::Cluster::join(&self, seeds, 2);
     ASSUME_ITS_TRUE(result == 0);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_join_with_null_self) {
+FOSSIL_TEST_CASE(cpp_cluster_test_join_with_null_self) {
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int result = fossil_network_cluster_join(NULL, seeds, 1);
+    int result = fossil::network::Cluster::join(nullptr, seeds, 1);
     ASSUME_ITS_TRUE(result == -1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_join_with_zero_seeds) {
+FOSSIL_TEST_CASE(cpp_cluster_test_join_with_zero_seeds) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
-    int result = fossil_network_cluster_join(&self, NULL, 0);
+    int result = fossil::network::Cluster::join(&self, nullptr, 0);
     ASSUME_ITS_TRUE(result == 0);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_broadcast_with_null_buffer) {
+FOSSIL_TEST_CASE(cpp_cluster_test_broadcast_with_null_buffer) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 1);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 1);
     ASSUME_ITS_TRUE(join_result == 0);
 
-    int result = fossil_network_cluster_broadcast(NULL, 10);
+    int result = fossil::network::Cluster::broadcast(nullptr, 10);
     ASSUME_ITS_TRUE(result == -1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_broadcast_with_zero_length) {
+FOSSIL_TEST_CASE(cpp_cluster_test_broadcast_with_zero_length) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 1);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 1);
     ASSUME_ITS_TRUE(join_result == 0);
 
     char dummy[4] = "abc";
-    int result = fossil_network_cluster_broadcast(dummy, 0);
+    int result = fossil::network::Cluster::broadcast(dummy, 0);
     ASSUME_ITS_TRUE(result == -1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_leave_with_valid_self) {
+FOSSIL_TEST_CASE(cpp_cluster_test_leave_with_valid_self) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 1);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 1);
     ASSUME_ITS_TRUE(join_result == 0);
 
-    int leave_result = fossil_network_cluster_leave(&self);
+    int leave_result = fossil::network::Cluster::leave(&self);
     ASSUME_ITS_TRUE(leave_result == 0);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_leave_with_null_self) {
-    int result = fossil_network_cluster_leave(NULL);
+FOSSIL_TEST_CASE(cpp_cluster_test_leave_with_null_self) {
+    int result = fossil::network::Cluster::leave(nullptr);
     ASSUME_ITS_TRUE(result == -1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_heartbeat_with_valid_self) {
+FOSSIL_TEST_CASE(cpp_cluster_test_heartbeat_with_valid_self) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 1);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 1);
     ASSUME_ITS_TRUE(join_result == 0);
 
-    int heartbeat_result = fossil_network_cluster_heartbeat(&self);
+    int heartbeat_result = fossil::network::Cluster::heartbeat(&self);
     ASSUME_ITS_TRUE(heartbeat_result == 0);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_heartbeat_with_null_self) {
-    int result = fossil_network_cluster_heartbeat(NULL);
+FOSSIL_TEST_CASE(cpp_cluster_test_heartbeat_with_null_self) {
+    int result = fossil::network::Cluster::heartbeat(nullptr);
     ASSUME_ITS_TRUE(result == -1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_get_active_nodes_with_valid_nodes) {
+FOSSIL_TEST_CASE(cpp_cluster_test_get_active_nodes_with_valid_nodes) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[2] = {
         { "node2", "127.0.0.2", 9002 },
         { "node3", "127.0.0.3", 9003 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 2);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 2);
     ASSUME_ITS_TRUE(join_result == 0);
 
     fossil_network_cluster_node_t nodes[4];
-    int active_count = fossil_network_cluster_get_active_nodes(nodes, 4);
+    int active_count = fossil::network::Cluster::get_active_nodes(nodes, 4);
     ASSUME_ITS_TRUE(active_count >= 1);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_get_active_nodes_with_null_nodes) {
-    int result = fossil_network_cluster_get_active_nodes(NULL, 4);
+FOSSIL_TEST_CASE(cpp_cluster_test_get_active_nodes_with_null_nodes) {
+    int result = fossil::network::Cluster::get_active_nodes(nullptr, 4);
     ASSUME_ITS_TRUE(result == 0);
 }
 
-FOSSIL_TEST_CASE(c_cluster_test_get_active_nodes_with_zero_max_nodes) {
+FOSSIL_TEST_CASE(cpp_cluster_test_get_active_nodes_with_zero_max_nodes) {
     fossil_network_cluster_node_t self = { "node1", "127.0.0.1", 9001 };
     fossil_network_cluster_node_t seeds[1] = {
         { "node2", "127.0.0.2", 9002 }
     };
-    int join_result = fossil_network_cluster_join(&self, seeds, 1);
+    int join_result = fossil::network::Cluster::join(&self, seeds, 1);
     ASSUME_ITS_TRUE(join_result == 0);
 
     fossil_network_cluster_node_t nodes[2];
-    int result = fossil_network_cluster_get_active_nodes(nodes, 0);
+    int result = fossil::network::Cluster::get_active_nodes(nodes, 0);
     ASSUME_ITS_TRUE(result == 0);
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
-FOSSIL_TEST_GROUP(c_cluster_tests) {
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_join_with_valid_self_and_seeds);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_join_with_null_self);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_join_with_zero_seeds);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_broadcast_with_null_buffer);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_broadcast_with_zero_length);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_leave_with_valid_self);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_leave_with_null_self);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_heartbeat_with_valid_self);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_heartbeat_with_null_self);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_get_active_nodes_with_valid_nodes);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_get_active_nodes_with_null_nodes);
-    FOSSIL_TEST_ADD(c_cluster_fixture, c_cluster_test_get_active_nodes_with_zero_max_nodes);
+FOSSIL_TEST_GROUP(cpp_cluster_tests) {
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_join_with_valid_self_and_seeds);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_join_with_null_self);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_join_with_zero_seeds);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_broadcast_with_null_buffer);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_broadcast_with_zero_length);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_leave_with_valid_self);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_leave_with_null_self);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_heartbeat_with_valid_self);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_heartbeat_with_null_self);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_get_active_nodes_with_valid_nodes);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_get_active_nodes_with_null_nodes);
+    FOSSIL_TEST_ADD(cpp_cluster_fixture, cpp_cluster_test_get_active_nodes_with_zero_max_nodes);
 
-    FOSSIL_TEST_REGISTER(c_cluster_fixture);
+    FOSSIL_TEST_REGISTER(cpp_cluster_fixture);
 } // end of tests
